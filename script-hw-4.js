2c422cc98b2d89b02d38e5526142800e0c4d485c
Anatoly Popov <popov.anatoly@gmail.com>
2019-01-20 21:31:14 +0300

    working on homework

---
+++ script-hw-4.js
@@ -0,0 +1,29 @@
+"use strict";
+
+function convertRegistry(number) {
+    let numRegistry = {};
+    if (isNaN(number)) {
+        console.log('number must be an integer')
+        return numRegistry;
+    } else if (number < 0 || number > 999) {
+        console.log('wrong number range (must be in [...999])')
+        return numRegistry;
+    } else {
+        numRegistry.firstDigit = number % 10;
+        numRegistry.secondDigit = (number % 100 - numRegistry.firstDigit) / 10;
+        number > 100 ? numRegistry.thirdDigit = (number - number % 100) / 100 :
+            numRegistry.thirdDigit = 0;
+        console.log(numRegistry);
+        return numRegistry;
+    }
+};
+convertRegistry(9);
+convertRegistry(89);
+convertRegistry(45);
+convertRegistry(301);
+convertRegistry(555);
+convertRegistry(999);
+convertRegistry('not a number');
+convertRegistry(-1000);
+convertRegistry(1500);
+
